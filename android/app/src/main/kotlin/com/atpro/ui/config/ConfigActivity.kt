4ebe240affbb1d025dae088380d0f7293f87bbd1
honghoaibao <honghoaibaoat@gmail.com>
2026-05-26 01:25:49 +0000

    ok

---
+++ android/app/src/main/kotlin/com/atpro/ui/config/ConfigActivity.kt
@@ -0,0 +1,22 @@
+package com.atpro.ui.config
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.material3.*
+import androidx.compose.ui.graphics.Color
+
+class ConfigActivity : AppCompatActivity() {
+    private val vm: ConfigViewModel by viewModels { ConfigViewModel.Factory(applicationContext) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.statusBarColor     = android.graphics.Color.TRANSPARENT
+        window.navigationBarColor = android.graphics.Color.TRANSPARENT
+        setContent {
+            MaterialTheme(darkColorScheme(
+                background = Color(0xFF0D0D14), surface = Color(0xFF1A1A2E),
+            )) { ConfigScreen(vm = vm) }
+        }
+    }
+}
